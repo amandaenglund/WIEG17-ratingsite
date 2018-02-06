9912b3c19468cc2500e6a4925d4101b70993b4da
amandaenglund <32718487+amandaenglund@users.noreply.github.com>
2018-02-06 14:27:58 +0100

    Bytte barnlåtar.se till childrensongs.com på tre ställen CHECK!

--- start.php
+++ start.php
@@ -3,7 +3,7 @@
 <?php
 require "./header.php";
 
-define("NAME", "Barnlåtar.se");
+define("NAME", "childrensongs.com");
 
 
 echo "<h1>Välkommen till " .NAME. "</h1><h2>" .NAME. " är en riktigt intressant site</h2><p>Här kan du följa en spännande ranking av Sveriges i särklass bästa barnlåtar. Välkommen till " .NAME. "</p>";
